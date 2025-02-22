ce29d9e7ad96c42ef8a8d0debc2fa10ba7c42d1f
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2025-02-22 02:12:22 -0600

    Filter

--- Tools/UserControl_Filter.Designer.cs
+++ Tools/UserControl_Filter.Designer.cs
@@ -28,17 +28,273 @@
         /// </summary>
         private void InitializeComponent()
         {
+            panel_NavBar = new Panel();
+            radioButton10 = new RadioButton();
+            radioButton1 = new RadioButton();
+            radioButton9 = new RadioButton();
+            radioButton2 = new RadioButton();
+            radioButton8 = new RadioButton();
+            radioButton5 = new RadioButton();
+            radioButton4 = new RadioButton();
+            radioButton7 = new RadioButton();
+            radioButton6 = new RadioButton();
+            radioButton3 = new RadioButton();
+            panel_TopBar = new Panel();
+            panel_Body = new Panel();
+            panel1 = new Panel();
+            progressBar1 = new ProgressBar();
+            panel_NavBar.SuspendLayout();
+            panel_Body.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
+            // 
+            // panel_NavBar
+            // 
+            panel_NavBar.BackColor = Color.White;
+            panel_NavBar.Controls.Add(radioButton10);
+            panel_NavBar.Controls.Add(radioButton1);
+            panel_NavBar.Controls.Add(radioButton9);
+            panel_NavBar.Controls.Add(radioButton2);
+            panel_NavBar.Controls.Add(radioButton8);
+            panel_NavBar.Controls.Add(radioButton5);
+            panel_NavBar.Controls.Add(radioButton4);
+            panel_NavBar.Controls.Add(radioButton7);
+            panel_NavBar.Controls.Add(radioButton6);
+            panel_NavBar.Controls.Add(radioButton3);
+            panel_NavBar.Dock = DockStyle.Left;
+            panel_NavBar.Location = new Point(0, 0);
+            panel_NavBar.Name = "panel_NavBar";
+            panel_NavBar.Size = new Size(186, 612);
+            panel_NavBar.TabIndex = 0;
+            // 
+            // radioButton10
+            // 
+            radioButton10.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton10.AutoSize = true;
+            radioButton10.BackColor = Color.Transparent;
+            radioButton10.Cursor = Cursors.Hand;
+            radioButton10.FlatAppearance.BorderSize = 0;
+            radioButton10.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton10.Location = new Point(28, 478);
+            radioButton10.Name = "radioButton10";
+            radioButton10.Size = new Size(141, 22);
+            radioButton10.TabIndex = 9;
+            radioButton10.TabStop = true;
+            radioButton10.Text = "radioButton10";
+            radioButton10.UseVisualStyleBackColor = false;
+            // 
+            // radioButton1
+            // 
+            radioButton1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton1.AutoSize = true;
+            radioButton1.BackColor = Color.Transparent;
+            radioButton1.Cursor = Cursors.Hand;
+            radioButton1.FlatAppearance.BorderSize = 0;
+            radioButton1.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton1.Location = new Point(38, 28);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(131, 22);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "radioButton1";
+            radioButton1.UseVisualStyleBackColor = false;
+            // 
+            // radioButton9
+            // 
+            radioButton9.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton9.AutoSize = true;
+            radioButton9.BackColor = Color.Transparent;
+            radioButton9.Cursor = Cursors.Hand;
+            radioButton9.FlatAppearance.BorderSize = 0;
+            radioButton9.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton9.Location = new Point(38, 428);
+            radioButton9.Name = "radioButton9";
+            radioButton9.Size = new Size(131, 22);
+            radioButton9.TabIndex = 8;
+            radioButton9.TabStop = true;
+            radioButton9.Text = "radioButton9";
+            radioButton9.UseVisualStyleBackColor = false;
+            // 
+            // radioButton2
+            // 
+            radioButton2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton2.AutoSize = true;
+            radioButton2.BackColor = Color.Transparent;
+            radioButton2.Cursor = Cursors.Hand;
+            radioButton2.FlatAppearance.BorderSize = 0;
+            radioButton2.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton2.Location = new Point(38, 78);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(131, 22);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "radioButton2";
+            radioButton2.UseVisualStyleBackColor = false;
+            // 
+            // radioButton8
+            // 
+            radioButton8.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton8.AutoSize = true;
+            radioButton8.BackColor = Color.Transparent;
+            radioButton8.Cursor = Cursors.Hand;
+            radioButton8.FlatAppearance.BorderSize = 0;
+            radioButton8.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton8.Location = new Point(38, 378);
+            radioButton8.Name = "radioButton8";
+            radioButton8.Size = new Size(131, 22);
+            radioButton8.TabIndex = 7;
+            radioButton8.TabStop = true;
+            radioButton8.Text = "radioButton8";
+            radioButton8.UseVisualStyleBackColor = false;
+            // 
+            // radioButton5
+            // 
+            radioButton5.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton5.AutoSize = true;
+            radioButton5.BackColor = Color.Transparent;
+            radioButton5.Cursor = Cursors.Hand;
+            radioButton5.FlatAppearance.BorderSize = 0;
+            radioButton5.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton5.Location = new Point(38, 228);
+            radioButton5.Name = "radioButton5";
+            radioButton5.Size = new Size(131, 22);
+            radioButton5.TabIndex = 4;
+            radioButton5.TabStop = true;
+            radioButton5.Text = "radioButton5";
+            radioButton5.UseVisualStyleBackColor = false;
+            // 
+            // radioButton4
+            // 
+            radioButton4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton4.AutoSize = true;
+            radioButton4.BackColor = Color.Transparent;
+            radioButton4.Cursor = Cursors.Hand;
+            radioButton4.FlatAppearance.BorderSize = 0;
+            radioButton4.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton4.Location = new Point(38, 178);
+            radioButton4.Name = "radioButton4";
+            radioButton4.Size = new Size(131, 22);
+            radioButton4.TabIndex = 3;
+            radioButton4.TabStop = true;
+            radioButton4.Text = "radioButton4";
+            radioButton4.UseVisualStyleBackColor = false;
+            // 
+            // radioButton7
+            // 
+            radioButton7.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton7.AutoSize = true;
+            radioButton7.BackColor = Color.Transparent;
+            radioButton7.Cursor = Cursors.Hand;
+            radioButton7.FlatAppearance.BorderSize = 0;
+            radioButton7.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton7.Location = new Point(38, 328);
+            radioButton7.Name = "radioButton7";
+            radioButton7.Size = new Size(131, 22);
+            radioButton7.TabIndex = 6;
+            radioButton7.TabStop = true;
+            radioButton7.Text = "radioButton7";
+            radioButton7.UseVisualStyleBackColor = false;
+            // 
+            // radioButton6
+            // 
+            radioButton6.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton6.AutoSize = true;
+            radioButton6.BackColor = Color.Transparent;
+            radioButton6.Cursor = Cursors.Hand;
+            radioButton6.FlatAppearance.BorderSize = 0;
+            radioButton6.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton6.Location = new Point(38, 278);
+            radioButton6.Name = "radioButton6";
+            radioButton6.Size = new Size(131, 22);
+            radioButton6.TabIndex = 5;
+            radioButton6.TabStop = true;
+            radioButton6.Text = "radioButton6";
+            radioButton6.UseVisualStyleBackColor = false;
+            // 
+            // radioButton3
+            // 
+            radioButton3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            radioButton3.AutoSize = true;
+            radioButton3.BackColor = Color.Transparent;
+            radioButton3.Cursor = Cursors.Hand;
+            radioButton3.FlatAppearance.BorderSize = 0;
+            radioButton3.Font = new Font("Arial Rounded MT Bold", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            radioButton3.Location = new Point(38, 128);
+            radioButton3.Name = "radioButton3";
+            radioButton3.Size = new Size(131, 22);
+            radioButton3.TabIndex = 2;
+            radioButton3.TabStop = true;
+            radioButton3.Text = "radioButton3";
+            radioButton3.UseVisualStyleBackColor = false;
+            // 
+            // panel_TopBar
+            // 
+            panel_TopBar.BackColor = SystemColors.Control;
+            panel_TopBar.Dock = DockStyle.Top;
+            panel_TopBar.Location = new Point(186, 0);
+            panel_TopBar.Name = "panel_TopBar";
+            panel_TopBar.Size = new Size(632, 50);
+            panel_TopBar.TabIndex = 1;
+            // 
+            // panel_Body
+            // 
+            panel_Body.BackColor = SystemColors.Control;
+            panel_Body.Controls.Add(panel1);
+            panel_Body.Dock = DockStyle.Fill;
+            panel_Body.Location = new Point(186, 50);
+            panel_Body.Name = "panel_Body";
+            panel_Body.Size = new Size(632, 562);
+            panel_Body.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(progressBar1);
+            panel1.Dock = DockStyle.Bottom;
+            panel1.Location = new Point(0, 526);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(632, 36);
+            panel1.TabIndex = 0;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            progressBar1.Location = new Point(3, 3);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new Size(626, 30);
+            progressBar1.TabIndex = 0;
             // 
             // UserControl_Filter
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel_Body);
+            Controls.Add(panel_TopBar);
+            Controls.Add(panel_NavBar);
             Name = "UserControl_Filter";
             Size = new Size(818, 612);
+            panel_NavBar.ResumeLayout(false);
+            panel_NavBar.PerformLayout();
+            panel_Body.ResumeLayout(false);
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
         #endregion
+
+        private Panel panel_NavBar;
+        private RadioButton radioButton10;
+        private RadioButton radioButton9;
+        private RadioButton radioButton8;
+        private RadioButton radioButton7;
+        private RadioButton radioButton6;
+        private RadioButton radioButton5;
+        private RadioButton radioButton4;
+        private RadioButton radioButton3;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Panel panel_Body;
+        private Panel panel_TopBar;
+        private Panel panel1;
+        private ProgressBar progressBar1;
     }
 }
--- Tools/UserControl_Filter.cs
+++ Tools/UserControl_Filter.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace Tools
 {
-    public partial class UserControl_Filter: UserControl
+    public partial class UserControl_Filter : UserControl
     {
         public UserControl_Filter()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            base.OnPaint(e); Graphics g = e.Graphics; int shadowSize = 20; // Adjust this to change shadow size
+                                                                           // Draw shadow
+            for (int i = 0; i < shadowSize; i++)
+            {
+                int alpha = Math.Max(0, 50 - (i * 5));
+                Color shadowColor = Color.FromArgb(alpha, Color.Black);
+                using (Pen pen = new Pen(shadowColor, 1))
+                {
+                    g.DrawRectangle(pen, new Rectangle(i, i, this.Width - i * 2, this.Height - i * 2));
+                }
+            }
+            // Check if Parent is not null
+            Color parentBackColor = this.Parent != null ? this.Parent.BackColor : Color.White;
+            // Draw original panel
+            g.FillRectangle(new SolidBrush(parentBackColor), new Rectangle(0, 0, this.Width, this.Height));
+            g.FillRectangle(Brushes.White, new Rectangle(shadowSize, shadowSize, this.Width - shadowSize * 2, this.Height - shadowSize * 2));
         }
     }
 }
