72e4421c331ea113ae5717690f3ff5999563a98e
Alan Isaac <136749111+Dev-Alan-Isaac@users.noreply.github.com>
2025-04-04 11:37:34 -0600

    Update UserControl_Extract.cs

--- Tools/UserControl_Extract.cs
+++ Tools/UserControl_Extract.cs
@@ -1,14 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Newtonsoft.Json.Linq;
-using System.Windows.Forms.VisualStyles;
+﻿using Newtonsoft.Json.Linq;
 using System.Diagnostics;
 
 namespace Tools
